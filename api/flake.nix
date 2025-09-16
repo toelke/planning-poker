b0c9ea92fcd9c97ba4f97c591046b825b7175c71
Philipp Riederer <philipp@riederer.email>
2025-09-16 11:37:41 +0200

    migrate to flake

---
+++ api/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ (pkgs.python311.withPackages (p: with p; [ pip uvicorn ])) ];
+          shellHook = ''
+            # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+            # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+            export PIP_PREFIX=$(pwd)/_build/pip_packages
+            export PYTHONPATH="$PIP_PREFIX/${pkgs.python311.sitePackages}:$PYTHONPATH"
+            export PATH="$PIP_PREFIX/bin:$PATH"
+            unset SOURCE_DATE_EPOCH
+          '';
+        };
+      });
+}
